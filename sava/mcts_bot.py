3050c42ce0ed20f5193ffa4aab52cff591ab3ada
Sava Sharif <savasharif@gmail.com>
2022-03-24 18:16:41 +0100

    New stuff

--- sava/mcts_bot.py
+++ sava/mcts_bot.py
@@ -8,7 +8,7 @@ import numpy as np
 from botbowl.ai.bots.random_bot import RandomBot
 
 from botbowl.core.game import Game
-from botbowl.core.model import Action, ActionChoice
+from botbowl import Action, ActionChoice, Square
 from botbowl.core.table import ActionType
 
 
@@ -17,6 +17,7 @@ class MctsBot(botbowl.Agent):
     def __init__(self, name, seed=None):
         super().__init__(name)
         self.my_team = None
+        self.opp_team = None
         self.rnd = np.random.RandomState(seed)
         self.evals = 0
 
@@ -41,24 +42,43 @@ class MctsBot(botbowl.Agent):
         def expand(self):
             self.expanded = True
             # Expand this node and set children accordingly.
-            for a in self.game.state.available_actions:
-                # Create child
-                # if a.action_type == ActionType.PLACE_PLAYER:
-                #     continue
-                
-                if a.players == None:
-                    pass
-
-                for player in a.players:
-                    sampled_pos = self.rnd.choice(a.positions, 5)
-                    for pos in sampled_pos:
-                        game_copy = deepcopy(self.game)
-                        action_taken = botbowl.Action(a.action_type, position=pos, player=player)
-                        game_copy._one_step(action_taken)
-                        # Flip step of other bot
-                        c = MctsBot.McNode(self.bot, game_copy, action_taken,self, self.team, self.rnd)
-                        c.playout()
-                        self.children.append(c)
+            for a in self.game.get_available_actions():
+                if a.players:
+                    for player in a.players:
+                        if a.positions:
+                            for pos in a.positions:
+                                    game_copy = deepcopy(self.game)
+                                    action_taken = botbowl.Action(a.action_type,position=pos,  player=player)
+                                    game_copy._one_step(action_taken)
+                                    # Flip step of other bot
+                                    c = MctsBot.McNode(self.bot, game_copy, action_taken,self, self.team, self.rnd)
+                                    c.playout()
+                                    self.children.append(c)
+                        else:
+                            game_copy = deepcopy(self.game)
+                            action_taken = botbowl.Action(a.action_type,  player=player)
+                            game_copy._one_step(action_taken)
+                            # Flip step of other bot
+                            c = MctsBot.McNode(self.bot, game_copy, action_taken,self, self.team, self.rnd)
+                            c.playout()
+                            self.children.append(c)
+                else:
+                    if a.positions:
+                        for pos in a.positions:
+                                game_copy = deepcopy(self.game)
+                                action_taken = botbowl.Action(a.action_type,position=pos)
+                                game_copy.step(action_taken)
+                                # Flip step of other bot
+                                c = MctsBot.McNode(self.bot, game_copy, action_taken,self, self.team, self.rnd)
+                                c.playout()
+                                self.children.append(c)
+                    game_copy = deepcopy(self.game)
+                    action_taken = botbowl.Action(a.action_type)
+                    game_copy._one_step(action_taken)
+                    # Flip step of other bot
+                    c = MctsBot.McNode(self.bot, game_copy, action_taken,self, self.team, self.rnd)
+                    c.playout()
+                    self.children.append(c)
 
         def UCT(self) -> float:
             # TODO C param
@@ -66,7 +86,8 @@ class MctsBot(botbowl.Agent):
 
         def find_best_child(self) -> Tuple[Self, bool]:
             if self.expanded:
-                return self.children[np.argmax([n.UCT() for n in self.children])], True
+                if self.children:
+                    return self.children[np.argmax([n.UCT() for n in self.children])], True
             return self, False
 
         def playout(self):
@@ -111,11 +132,52 @@ class MctsBot(botbowl.Agent):
 
     def new_game(self, game, team):
         self.my_team = team
+        self.opp_team = game.get_opp_team(team)
+
 
     def act(self, game):
         # Build tree
         
         # Select a random action type
+
+        
+
+        available_actions = [elem.action_type for elem in game.get_available_actions()]
+        print(available_actions)
+        if botbowl.ActionType.PLACE_BALL in available_actions:
+            left_center = Square(7, 8)
+            right_center = Square(20, 8)
+            if game.is_team_side(left_center, self.opp_team):
+                return Action(ActionType.PLACE_BALL, position=left_center)
+            return Action(ActionType.PLACE_BALL, position=right_center)
+
+
+        if len(available_actions) == 1:
+            return Action(available_actions[0])
+
+        # handle heads or tail
+        if botbowl.ActionType.HEADS in available_actions or botbowl.ActionType.TAILS in available_actions:
+            return np.random.choice([Action(botbowl.ActionType.HEADS), Action(botbowl.ActionType.TAILS)])
+
+        # handle kick or receive
+        if botbowl.ActionType.KICK in available_actions or botbowl.ActionType.RECEIVE in available_actions:
+            return np.random.choice([Action(botbowl.ActionType.KICK), Action(botbowl.ActionType.RECEIVE)])
+
+        if botbowl.ActionType.MOVE in available_actions:
+             available_actions.remove(botbowl.ActionType.MOVE)
+
+
+        if botbowl.ActionType.PLACE_PLAYER in available_actions or botbowl.ActionType.END_SETUP in available_actions or botbowl.ActionType.SETUP_FORMATION_SPREAD in available_actions or botbowl.ActionType.SETUP_FORMATION_WEDGE in available_actions:
+            available_actions.remove(botbowl.ActionType.PLACE_PLAYER)
+            for elem in game.get_players_on_pitch(team=self.my_team):
+                return Action(botbowl.ActionType.END_SETUP)
+            available_actions.remove(botbowl.ActionType.END_SETUP)
+            return Action(np.random.choice(available_actions))
+        
+
+
+
+            
 
         root = MctsBot.McNode(self, game, None, None, self.my_team, self.rnd)
         while self.evals < 100:
@@ -132,26 +194,11 @@ class MctsBot(botbowl.Agent):
             for p in path:
                 p.n_simulations += 1
                 if c.result == 1:
-                    p.n_simulations += 1
+                    p.n_wins += 1
 
         best, _ = root.find_best_child()
+        print(best.action)
         return best.action
-            
-        # while True:
-        #     action_choice = self.rnd.choice(game.state.available_actions)
-        #     # Ignore PLACE_PLAYER actions
-        #     if action_choice.action_type != botbowl.ActionType.PLACE_PLAYER:
-        #         break
-
-        # # Select a random position and/or player
-        # position = self.rnd.choice(action_choice.positions) if len(action_choice.positions) > 0 else None
-        # player = self.rnd.choice(action_choice.players) if len(action_choice.players) > 0 else None
-
-        # # Make action object
-        # action = botbowl.Action(action_choice.action_type, position=position, player=player)
-
-        # # Return action to the framework
-        # return action
 
     def end_game(self, game):
         pass
