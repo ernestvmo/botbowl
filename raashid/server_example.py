46eda611ba35ce26ead6e00118fde2d53f1b3f63
ernestvmo <ernestvmo@gmail.com>
2022-04-08 19:57:26 +0200

    removed sava and raashid folders

--- raashid/server_example.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# from MCTS_bot import *
-from MCTS_bot_updated import *
-
-import botbowl.web.server as server
-
-if __name__ == "__main__":
-    server.start_server(debug=True, use_reloader=False, port=1234)
